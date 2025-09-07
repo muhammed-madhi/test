8c087cf3254708fb4cd6404918fbb3998ee03241
muhammed-madhi <85786132+muhammed-madhi@users.noreply.github.com>
2025-09-07 11:14:33 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{padding:20, fontFamily:'system-ui'}}>
+      <img src="/logo.png" alt="Organization logo"
+           style={{position:'fixed', top:16, insetInlineStart:16, height:48}} />
+      <h1 style={{marginTop:80}}>حياكم الله!</h1>
+    </main>
+  );
+}
